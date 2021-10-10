fb1ffebe986beac448033762130d78e7f198a19e
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-10-09 22:45:32 -0700

    navbar styling, media query for mobile complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,9 +6,13 @@ import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
 
+//Components
+import Navbar from './components/Navbar'
+
 function App() {
   return (
     <Router>
+      <Navbar />
       <main>
         <Switch>
           <Route exact path="/" component={HomeScreen}/>
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -10,11 +10,25 @@ const Navbar = () => {
       </div>
       <ul className="Navbar-links">
         <li>
-          <Link to="/cart">
-            
+          <Link to="/cart" className="Cart-link">
+            <i className="fas fa-shopping-cart"></i>
+            <span>
+              Cart
+              <span className="Cart-total">0</span>
+            </span>
+          </Link>
+        </li>
+        <li>
+          <Link to="/">
+            Shop
           </Link>
         </li>
       </ul>
+      <div className="Hamburger-menu">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </nav>
   )
 }
