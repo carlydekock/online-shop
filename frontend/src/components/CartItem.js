498c2e215b67a1533db056fa8513973e04db7b59
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-10-10 16:58:38 -0700

    cart item rendering with static info

---
+++ frontend/src/components/CartItem.js
@@ -0,0 +1,27 @@
+import './CartItem.css';
+import {Link} from 'react-router-dom';
+
+const CartItem = () => {
+  return (
+    <div className="CartItem">
+      <div className="CartItem-image">
+        <img src="../img/tent.png" alt="Produt name"/>
+      </div>
+      <Link to={`/product/${111}`} className="CartItem-name">
+        <p>Product 1</p>
+      </Link>
+      <p className="CartItem-price">$499.99</p>
+      <select className="CartItem-select">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+      </select>
+      <button className="CartItem-delete">
+        <i className="fas fa-trash"></i>
+      </button>
+    </div>
+  )
+}
+
+export default CartItem;
