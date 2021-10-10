f7df79a533030bb533504aa90b0ecd911f5bb5d0
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-10-10 15:37:37 -0700

    added breakpoints for columns of products displayed

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,18 @@
 import './HomeScreen.css';
+import Product from '../components/Product';
 
 const HomeScreen = () => {
   return (
     <div className="HomeScreen">
-      HomeScreen
+      <h2 className="HomeScreen-title">Latest Products</h2>
+      <div className="HomeScreen-products">
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+      </div>
     </div>
   )
 }
