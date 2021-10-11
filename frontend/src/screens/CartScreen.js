1a92e2508eda01765e8929c1b1ba05c0055e6f5c
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-10-10 18:46:20 -0700

    cart page styling complete

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,9 +1,25 @@
 import './CartScreen.css';
+import CartItem from '../components/CartItem';
 
 const CartScreen = () => {
   return (
     <div className="CartScreen">
-      Cart Screen
+      <div className="CartScreen-left">
+        <h2>Shopping Cart</h2>
+        <CartItem />
+        <CartItem />
+        <CartItem />
+        <CartItem />
+      </div>
+      <div className="CartScreen-right">
+        <div className="CartScreen-info">
+          <p>Subtotal (0) items</p>
+          <p>$499.99</p>
+        </div>
+        <div>
+          <button>Proceed To Checkout</button>
+        </div>
+      </div>
     </div>
   )
 }
